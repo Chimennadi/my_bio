24729791c01cbc3114f7480b0d1cda391e2ea901
Nnadi .C Charles <chimennadi66@gmail.com>
2022-10-17 09:54:35 +0100

    Update MainActivity.java

--- app/src/main/java/com/chime/bio/MainActivity.java
+++ app/src/main/java/com/chime/bio/MainActivity.java
@@ -9,19 +9,29 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 
+//using databinding
+//import com.chime.bio.databinding.ActivityMainBinding;
+
+
 public class MainActivity extends AppCompatActivity {
+    //private ActivityMainBinding binding;
     private EditText enterHobbies;
     private  TextView hobbies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        //binding.doneButton.setOnClickListener(this::addHobbies);
         setContentView(R.layout.activity_main);
         enterHobbies = findViewById(R.id.enter_hobbies);
         hobbies = findViewById(R.id.hobbies_text);
     }
 
     public void addHobbies(View view) {
+        //binding.hobbiesText.setText(String.format("Hobbies: %s", binding.enterHobbies.getText().toString().trim()));
+        //binding.invalidateAll();
+        //binding.hobbiesText.setVisibility(View.VISIBLE);
         hobbies.setText(String.format("Hobbies: %s", enterHobbies.getText().toString().trim()));
         hobbies.setVisibility(View.VISIBLE);
         enterHobbies.setText("");
